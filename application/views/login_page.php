2b6bf265b4f4d39392aa13bb01cbeca7a6c9d077
Thomas <thomas.vitiello@outlook.fr>
2017-07-18 16:38:23 +0200

    integration formulaires connection/inscription

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -14,21 +14,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="login-page">
     <div class="form">
         <form class="register-form">
-            <input type="text" placeholder="name"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="pseudo"/>
+            <input type="password" placeholder="mot de passe"/>
             <input type="text" placeholder="email address"/>
             <button>create</button>
             <p class="message">Already registered? <a href="#">Sign In</a></p>
         </form>
         <form class="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>login</button>
-            <p class="message">Not registered? <a href="#">Create an account</a></p>
+            <input type="text" placeholder="pseudo"/>
+            <input type="password" placeholder="mot de passe"/>
+            <button>Se connecter</button>
+            <p class="message">Pas enregistré? <a href="#">Créez un compte</a></p>
         </form>
     </div>
 </div>
-
+<script src="<?php echo base_url();?>public/javascript/jquery-3.2.1.min.js"></script>
 <script>
     $('.message a').click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
