a95ef3be31b6c6ec4e6618e365d3e22847a4edba
Thomas <thomas.vitiello@outlook.fr>
2017-07-19 15:38:59 +0200

    ajout du model de login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,4 +8,42 @@ class Home extends CI_Controller
     {
         $this->load->view('login_page');
     }
+
+    // gestion de l'enregistrement d'un user
+    public function register_validation(){
+        $this->form_validation->set_rules('pseudo_register', '"pseudo"', 'trim|required|xss_clean|min_length[5]|alpha_numeric');
+
+        $this->form_validation->set_rules('password_register', '"mot de passe"', 'trim|required|xss_clean|min_length[5]');
+
+        $this->form_validation->set_rules('email_register', '"adresse mail"', 'trim|required|valid_email|is_unique[users.email]|xss_clean');
+
+        if ($this->form_validation->run() == false) {
+
+            $data['register'] = true;
+
+            $this->load->view('login_page', $data);
+
+        }else {
+            die('form ok');
+            $this->load->view('user/compte_utilisateur');
+
+        }
+    }
+
+    // gestion de la connection d'un user
+    public function login_validation(){
+        $this->form_validation->set_rules('pseudo_login', '"pseudo"', 'trim|required|xss_clean|alpha_numeric');
+
+        $this->form_validation->set_rules('password_login', '"mot de passe"', 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('login_page');
+
+        }else {
+            die('form ok');
+            $this->load->view('user/compte_utilisateur');
+
+        }
+    }
 }
--- application/views/login_page.php
+++ application/views/login_page.php
@@ -15,15 +15,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="form">
         <?= form_open('home/register_validation', array('class' => 'register-form')); ?>
             <?php
-                $pseudo = array('name'=>'pseudo', 'placeholder'=>'pseudo');
+                echo form_error('pseudo_register');
+                $pseudo = array('name'=>'pseudo_register', 'placeholder'=>'pseudo', 'value'=>set_value('pseudo_register'));
                 echo form_input($pseudo);
             ?>
             <?php
-                $password = array('name'=>'password', 'placeholder'=>'mot de passe');
+                echo form_error('password_register');
+                $password = array('name'=>'password_register', 'placeholder'=>'mot de passe', 'value'=>set_value('password_register'), 'type' => 'password');
                 echo form_input($password);
             ?>
             <?php
-                $email = array('name'=>'email', 'placeholder'=>'Adresse mail');
+                echo form_error('email_register');
+                $email = array('name'=>'email_register', 'placeholder'=>'Adresse mail', 'value'=>set_value('email_register'));
                 echo form_input($email);
             ?>
             <?= form_submit('submit', 'Enregistrer'); ?>
@@ -31,11 +34,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <?= form_close(); ?>
         <?= form_open('home/login_validation', array('class' => 'login-form')); ?>
             <?php
-                $pseudo = array('name'=>'pseudo', 'placeholder'=>'pseudo');
+                echo form_error('pseudo_login');
+                $pseudo = array('name'=>'pseudo_login', 'placeholder'=>'pseudo', 'value'=>set_value('pseudo_login'));
                 echo form_input($pseudo);
             ?>
             <?php
-                $password = array('name'=>'password', 'placeholder'=>'mot de passe');
+                echo form_error('password_login');
+                $password = array('name'=>'password_login', 'placeholder'=>'mot de passe', 'value'=>set_value('password_login'), 'type' => 'password');
                 echo form_input($password);
             ?>
             <?= form_submit('submit', 'Se connecter'); ?>
@@ -44,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 </div>
 <script src="<?php echo base_url();?>public/javascript/jquery-3.2.1.min.js"></script>
+
+<?php if(isset($register) && $register == true){
+    ?>
+    <script> $('form').animate({height: "toggle", opacity: "toggle"}, 1); </script>
+    <?php
+} ?>
+
 <script>
     $('.message a').click(function(){
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
