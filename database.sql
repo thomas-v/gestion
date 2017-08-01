dafcc1716c51c040100f36594789d98eec98bcd6
Thomas <thomas.vitiello@outlook.fr>
2017-08-01 03:37:27 +0200

    ajout table category

--- database.sql
+++ database.sql
@@ -23,3 +23,11 @@ CREATE TABLE IF NOT EXISTS `statistical` (
   `user_ip` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` datetime DEFAULT NOW(),
+  PRIMARY KEY (`id`)
+);
